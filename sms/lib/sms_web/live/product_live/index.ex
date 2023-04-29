13cb7288fd12045a8a2ac74f833c4ae1272027c8
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-04-28 19:50:55 -0700

    finished all changes?

--- sms/lib/sms_web/live/product_live/index.ex
+++ sms/lib/sms_web/live/product_live/index.ex
@@ -4,6 +4,10 @@ defmodule SmsWeb.ProductLive.Index do
   alias Sms.Items
   alias Sms.Items.Product
 
+  # includes
+  alias Sms.Management
+  alias Sms.Management.Store
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, stream(socket, :products, Items.list_products())}
@@ -18,12 +22,14 @@ defmodule SmsWeb.ProductLive.Index do
     socket
     |> assign(:page_title, "Edit Product")
     |> assign(:product, Items.get_product!(id))
+    |> assign(:stores, Management.list_stores()) # lists all stores
   end
 
   defp apply_action(socket, :new, _params) do
     socket
     |> assign(:page_title, "New Product")
     |> assign(:product, %Product{})
+    |> assign(:stores, Management.list_stores()) # lists all stores
   end
 
   defp apply_action(socket, :index, _params) do
@@ -34,7 +40,7 @@ defmodule SmsWeb.ProductLive.Index do
 
   @impl true
   def handle_info({SmsWeb.ProductLive.FormComponent, {:saved, product}}, socket) do
-    {:noreply, stream_insert(socket, :products, product)}
+    {:noreply, stream_insert(socket, :products, product |> Repo.preload(:store, force: true))} # preloads the store
   end
 
   @impl true
